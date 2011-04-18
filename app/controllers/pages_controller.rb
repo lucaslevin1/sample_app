23430ea54ae2a90ab24c77d8a94717ed23cd2b08
Lucas Levin <lucaslevin1@gmail.com>
2011-04-17 22:26:44 -0400

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
   
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contanct"
   end
+  
+  def about
+    @title = "About"
+  end
+  
 end
